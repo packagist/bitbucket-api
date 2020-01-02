a1c78e8ffd591af4377ebda7771f699b20e3b935
Stephan Vock <stephan.vock@gmail.com>
2020-01-02 17:44:18 +0000

    Emails: removed deprecated/removed endpoints, switcht to available v2 endpoints

--- test/Bitbucket/Tests/API/Users/EmailsTest.php
+++ test/Bitbucket/Tests/API/Users/EmailsTest.php
@@ -12,13 +12,14 @@ class EmailsTest extends Tests\TestCase
         $endpoint       = 'users/gentle/emails';
         $expectedResult = json_encode('dummy');
 
-        $emails = $this->getApiMock('Bitbucket\API\Users\Emails');
-        $emails->expects($this->once())
-            ->method('requestGet')
+        $client = $this->getHttpClientMock();
+        $client->expects($this->once())
+            ->method('get')
             ->with($endpoint)
-            ->will( $this->returnValue($expectedResult) );
+            ->willReturn($expectedResult);
 
         /** @var $emails \Bitbucket\API\Users\Emails */
+        $emails = $this->getClassMock('Bitbucket\API\Users\Emails', $client);
         $actual = $emails->all('gentle');
 
         $this->assertEquals($expectedResult, $actual);
@@ -29,55 +30,17 @@ class EmailsTest extends Tests\TestCase
         $endpoint       = 'users/gentle/emails/dummy@example.com';
         $expectedResult = json_encode('dummy');
 
-        $emails = $this->getApiMock('Bitbucket\API\Users\Emails');
-        $emails->expects($this->once())
-            ->method('requestGet')
+        $client = $this->getHttpClientMock();
+        $client->expects($this->once())
+            ->method('get')
             ->with($endpoint)
-            ->will( $this->returnValue($expectedResult) );
+            ->willReturn($expectedResult);
 
         /** @var $emails \Bitbucket\API\Users\Emails */
+        $emails = $this->getClassMock('Bitbucket\API\Users\Emails', $client);
+
         $actual = $emails->get('gentle', 'dummy@example.com');
 
         $this->assertEquals($expectedResult, $actual);
-    }
-
-    public function testAddNewEmailSuccess()
-    {
-        $endpoint   = 'users/gentle/emails/dummy@example.com';
-
-        $email = $this->getApiMock('\Bitbucket\API\Users\Emails');
-        $email->expects($this->once())
-            ->method('requestPost')
-            ->with($endpoint);
-
-        /** @var $email \Bitbucket\API\Users\Emails */
-        $email->create('gentle', 'dummy@example.com');
-    }
-
-    public function testUpdateEmailSuccess()
-    {
-        $endpoint   = 'users/gentle/emails/dummy@example.com';
-        $params     = array('primary' => true);
-
-        $email = $this->getApiMock('\Bitbucket\API\Users\Emails');
-        $email->expects($this->once())
-            ->method('requestPut')
-            ->with($endpoint, $params);
-
-        /** @var $email \Bitbucket\API\Users\Emails */
-        $email->update('gentle', 'dummy@example.com', true);
-    }
-
-    public function testDeleteEmailSuccess()
-    {
-        $endpoint   = 'users/gentle/emails/dummy@example.com';
-
-        $email = $this->getApiMock('\Bitbucket\API\Users\Emails');
-        $email->expects($this->once())
-            ->method('requestDelete')
-            ->with($endpoint);
-
-        /** @var $email \Bitbucket\API\Users\Emails */
-        $email->delete('gentle', 'dummy@example.com');
     }
 }
