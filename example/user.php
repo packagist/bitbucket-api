3195e20567d45c557e8bb208938cd174ec028ac7
Stephan Vock <stephan.vock@gmail.com>
2020-01-03 21:22:55 +0000

    User: remove deprecated/removed endpoints

--- example/user.php
+++ example/user.php
@@ -22,18 +22,6 @@ $user->setCredentials( new Bitbucket\API\Authentication\Basic($bb_user, $bb_pass
 # get user profile
 # print_r($user->get());
 
-# update user profile
-/*print_r($user->update(array(
-    'first_name'    => 'John',
-    'last_name'     => 'Doe'
-)));*/
-
-# get user privileges
-#print_r($user->privileges());
-
-# get a list of repositories an account follows
-#print_r($user->follows());
-
 # get a list of repositories visible to an account
 #print_r($user->repositories()->get());
 
--- lib/Bitbucket/API/User.php
+++ lib/Bitbucket/API/User.php
@@ -30,43 +30,7 @@ class User extends Api
      */
     public function get()
     {
-        return $this->getClient()->get('user/');
-    }
-
-    /**
-     * Update user
-     *
-     * @access public
-     * @param  array            $options Filed->value pair of account settings
-     * @return MessageInterface
-     *
-     * @see https://confluence.atlassian.com/display/BITBUCKET/user+Endpoint#userEndpoint-Updateauser
-     */
-    public function update(array $options)
-    {
-        return $this->requestPut('user/', $options);
-    }
-
-    /**
-     * Get a list of user privileges
-     *
-     * @access public
-     * @return MessageInterface
-     */
-    public function privileges()
-    {
-        return $this->requestGet('user/privileges');
-    }
-
-    /**
-     * Get a list of repositories an account follows
-     *
-     * @access public
-     * @return MessageInterface
-     */
-    public function follows()
-    {
-        return $this->requestGet('user/follows');
+        return $this->getClient()->setApiVersion('2.0')->get('user/');
     }
 
     /**
